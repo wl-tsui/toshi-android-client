a886dd3cb65eec6897fc03452081ae8c134f8699
Ellen Shapiro <designatednerd@gmail.com>
2018-04-04 16:06:44 -0400

    rm unused import

--- app/src/main/java/com/toshi/view/adapter/SearchHeaderAdapter.kt
+++ app/src/main/java/com/toshi/view/adapter/SearchHeaderAdapter.kt
@@ -2,7 +2,6 @@ package com.toshi.view.adapter
 
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
 import com.toshi.R
 import com.toshi.view.adapter.viewholder.SearchHeaderViewHolder
